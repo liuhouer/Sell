27a45d5bf9a4cfd82be61b9ba309a327fb4be8e8
test01 <1434088223@qq.com>
2018-07-03 17:03:38 +0800

    增加了文件上传下载的功能

--- src/main/java/com/example/sell/SellApplication.java
+++ src/main/java/com/example/sell/SellApplication.java
@@ -2,7 +2,17 @@ package com.example.sell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @SpringBootApplication
 //使用redis缓存
@@ -12,4 +22,19 @@ public class SellApplication {
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
     }
+
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
+
 }
---
+++ src/main/java/com/example/sell/controller/FileController.java
@@ -0,0 +1,83 @@
+package com.example.sell.controller;
+
+import com.example.sell.utils.FileUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+//@RequestMapping("/buyer/product")
+public class FileController {
+    //跳转到上传文件的页面
+    @RequestMapping(value = "/gouploadimg", method = RequestMethod.GET)
+    public String goUploadImg() {
+        //跳转到 templates 目录下的 uploadimg.html
+        return "uploadimg";
+    }
+
+    //处理文件上传
+    @RequestMapping(value = "/testuploadimg", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadImg(@RequestParam("file") MultipartFile file,
+                     HttpServletRequest request) {
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        /*System.out.println("fileName-->" + fileName);
+        System.out.println("getContentType-->" + contentType);*/
+//        String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+        String filePath = "G:/webproject/sell/uploadfile/";
+        try {
+            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+
+        }
+        //返回json
+        return filePath;
+    }
+
+    @RequestMapping(value = "download", method = RequestMethod.GET)
+    public String downLoad(HttpServletResponse response) {
+        String fileName = "a.jpeg";
+        String filePath = "F:/test";
+        File file = new File("G:/webproject/sell/uploadfile/a.jpeg");
+        FileInputStream in = null;
+        OutputStream out = null;
+        try {
+            response.setContentType("application/force-download");
+            if (fileName.lastIndexOf("\\") > 0) {
+                fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
+            }
+            fileName = java.net.URLEncoder.encode(fileName, "UTF-8");
+            response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+            out = response.getOutputStream();
+            byte d[] = new byte[256];
+            int count = 0;
+            in = new FileInputStream(file);
+            while ((count = in.read(d)) > 0) {
+                out.write(d, 0, count);
+                out.flush();
+            }
+        } catch (IOException e) {
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+}
